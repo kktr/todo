07ae702ccdcc484bd9795a8773a88219f811dfc9
Krystian Kat <krystian.kat@protonmail.com>
2022-04-09 15:51:50 +0200

    refactor: create better names and classNames

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,203 +5,244 @@ import styles from '../styles/Home.module.css';
 type ITaskList = ITask[];
 interface ITask {
   id: number;
-  text: string;
-  done: boolean;
+  title: string;
+  description?: string;
+  completed: boolean;
   isEdit: boolean;
 }
 
 enum TaskType {
   All = 'ALL',
-  Done = 'DONE',
-  Undone = 'UNDONE',
+  Completed = 'COMPLETED',
+  Uncompleted = 'UNCOMPLETED',
 }
 
 const Home: NextPage = () => {
-  const [enteredText, setEnteredText] = useState<string>('');
-  const [editedTask, setEditedTask] = useState<ITask>();
-  const [textEditedTask, setTextEditedTask] = useState<string | null>(null);
   const [tasksList, setTasksList] = useState<ITaskList | null>(null);
-  const [isEdit, setIsEdit] = useState<boolean>(false);
   const [tasksTypeToDisplay, setTasksTypeToDisplay] = useState<TaskType>(
     TaskType.All
   );
+  const [enteredTitleNewTask, setEnteredTitleNewTask] = useState<string>('');
+  const [enteredTitleEditedTask, setEnteredTitleEditedTask] = useState<
+    string | null
+  >(null);
+  const [editedTask, setEditedTask] = useState<ITask>();
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [hightestId, setHightestId] = useState<number>(0);
 
-  const textChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setEnteredText(event.target.value);
+  const titleChangeNewTaskHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredTitleNewTask(event.target.value);
   };
 
-  const todoEditHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setTextEditedTask(event.target.value);
+  const titleChangeEditedTaskHandler = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnteredTitleEditedTask(event.target.value);
   };
 
-  const submitNewItem = (e: React.FormEvent<HTMLFormElement>) => {
+  const submitNewTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    addTodo(enteredText, false);
-    setEnteredText('');
+    addTask(enteredTitleNewTask);
+    setEnteredTitleNewTask('');
   };
 
-  const addTodo = (newTodo: string, isDone: boolean) => {
-    if (tasksList !== null && tasksList.length !== 0) {
-      let newId = tasksList[tasksList.length - 1].id + 1;
+  const addTask = (newTaskTitle: string, isCompleted: boolean = false) => {
+    let newId = hightestId + 1;
+
+    if (tasksList !== null && tasksList.length > 0) {
       setTasksList([
         ...tasksList,
-        { id: newId, text: newTodo, done: isDone, isEdit: false },
+        {
+          id: newId,
+          title: newTaskTitle,
+          completed: isCompleted,
+          isEdit: false,
+        },
       ]);
     } else {
-      let newId = 0;
-      setTasksList([{ id: newId, text: newTodo, done: isDone, isEdit: false }]);
+      setTasksList([
+        {
+          id: newId,
+          title: newTaskTitle,
+          completed: isCompleted,
+          isEdit: false,
+        },
+      ]);
     }
+    setHightestId((previousHightestId) => previousHightestId + 1);
   };
 
-  const editArray = (
-    editedArray: any[],
-    replacedItem: { id: any; text?: string; done?: boolean; isEdit?: boolean }
+  const editTaskInTasksList = (
+    editedTaskList: any[],
+    replacedTask: {
+      id: any;
+      title?: string;
+      completed?: boolean;
+      isEdit?: boolean;
+    }
   ) => {
-    return editedArray.map((element) => {
-      if (element.id === replacedItem.id) {
-        return replacedItem;
-      } else return element;
+    return editedTaskList.map((task) => {
+      return task.id === replacedTask.id ? replacedTask : task;
     });
   };
 
-  const deleteTodo = (tasksList, deletedTodo) => {
-    return tasksList.filter((todo) => todo.id !== deletedTodo.id);
+  const deleteTaskInTasksList = (editedTaskList: any[], deletedTask: ITask) => {
+    return editedTaskList.filter(
+      (Task: { id: number }) => Task.id !== deletedTask.id
+    );
   };
 
   return (
     <div className={styles.container}>
       <h1>tasksList</h1>
 
-      <form onSubmit={submitNewItem}>
-        <div>
-          <div className={styles.control}>
-            <label htmlFor="todo" className={styles.label}>
-              add todo
-            </label>
+      <form onSubmit={submitNewTask}>
+        <div className={styles.control}>
+          <label htmlFor="newTask" className={styles.label}>
+            add task
+          </label>
 
-            <input
-              id="todo"
-              type="text"
-              required
-              value={enteredText}
-              onChange={textChangeHandler}
-              className={`${styles.input} form-control`}
-            />
-          </div>
+          <input
+            id="newTask"
+            type="title"
+            required
+            value={enteredTitleNewTask}
+            onChange={titleChangeNewTaskHandler}
+            className={`${styles.input} form-control`}
+          />
         </div>
         <div className="">
-          <button id="submit-form" type="submit" className={styles.button}>
+          <button id="submit-new-task" type="submit" className={styles.button}>
             ADD
           </button>
         </div>
       </form>
 
-      <div>
+      <ul>
         {tasksList &&
-          tasksList.map((todo, index) => {
-            if (tasksTypeToDisplay === 'done' && todo.done === false) return;
-            if (tasksTypeToDisplay === 'todo' && todo.done === true) return;
-            return !todo.isEdit ? (
-              <div key={todo.id}>
+          tasksList.map((task, index) => {
+            if (
+              tasksTypeToDisplay === TaskType.Completed &&
+              task.completed === false
+            )
+              return;
+            if (
+              tasksTypeToDisplay === TaskType.Uncompleted &&
+              task.completed === true
+            )
+              return;
+            return !task.isEdit ? (
+              <li key={`task task--${task.id}`}>
                 <div
-                  id={`todo-${todo.id}`}
-                  className={todo.done ? 'finished' : 'unfinished'}
+                  className={`task_name task_name--${task.id} ${
+                    task.completed
+                      ? 'task_name--completed'
+                      : 'task_name--uncompleted'
+                  }`}
                 >
-                  {todo.text}
+                  {task.title}
                 </div>
-                <button
-                  onClick={() => {
-                    const oldTodo = tasksList[index];
-
-                    setTasksList(
-                      editArray(tasksList, {
-                        id: oldTodo.id,
-                        text: oldTodo.text,
-                        done: !oldTodo.done,
-                        isEdit: false,
-                      })
-                    );
-                  }}
+                <div
+                  className={`task_control-panel task_control-panel--${task.id}`}
                 >
-                  done
-                </button>
-                <button
-                  onClick={() => {
-                    if (isEdit) return;
-                    setIsEdit(true);
+                  <button
+                    className={`task_button task_button--done`}
+                    onClick={() => {
+                      const oldTask = tasksList[index];
 
-                    const oldTodo = tasksList[index];
+                      console.log(!oldTask.completed);
+                      setTasksList(
+                        editTaskInTasksList(tasksList, {
+                          ...oldTask,
+                          completed: !oldTask.completed,
+                        })
+                      );
+                    }}
+                  >
+                    done
+                  </button>
+                  <button
+                    className={`task_button task_button--edit`}
+                    onClick={() => {
+                      if (isEdit) return;
 
-                    setTasksList(
-                      editArray(tasksList, {
-                        id: oldTodo.id,
-                        text: oldTodo.text,
-                        done: oldTodo.done,
-                        isEdit: true,
-                      })
-                    );
-                  }}
-                >
-                  edit
-                </button>
-                <button
-                  onClick={() => {
-                    setTasksList(deleteTodo(tasksList, tasksList[index]));
-                  }}
-                >
-                  delete
-                </button>
-              </div>
+                      const oldTask = tasksList[index];
+                      setIsEdit(true);
+
+                      setTasksList(
+                        editTaskInTasksList(tasksList, {
+                          ...oldTask,
+                          isEdit: true,
+                        })
+                      );
+                    }}
+                  >
+                    edit
+                  </button>
+                  <button
+                    className={`task_button task_button--delete`}
+                    onClick={() => {
+                      setTasksList(
+                        deleteTaskInTasksList(tasksList, tasksList[index])
+                      );
+                    }}
+                  >
+                    delete
+                  </button>
+                </div>
+              </li>
             ) : (
               <form
+                key={`form-change-title--task-${task.id}`}
+                className={`form-change-title form-change-title--task-${task.id}`}
                 onSubmit={() => {
-                  const oldTodo = tasksList[index];
+                  const oldTask = tasksList[index];
                   setIsEdit(false);
 
                   setTasksList(
-                    editArray(tasksList, {
-                      id: oldTodo.id,
-                      text:
-                        textEditedTask !== null ? textEditedTask : oldTodo.text,
-                      done: oldTodo.done,
+                    editTaskInTasksList(tasksList, {
+                      ...oldTask,
+                      title:
+                        enteredTitleEditedTask !== null
+                          ? enteredTitleEditedTask
+                          : oldTask.title,
                       isEdit: false,
                     })
                   );
 
-                  setTextEditedTask(null);
+                  setEnteredTitleEditedTask(null);
                 }}
               >
-                <div>
-                  <div className={styles.control}>
-                    <label htmlFor="editedTodo" className={styles.label}>
-                      edit:
-                    </label>
+                <div className={styles.control}>
+                  <label htmlFor="editedTask" className={styles.label}>
+                    edit:
+                  </label>
 
-                    <input
-                      id="editedTodo"
-                      type="text"
-                      required
-                      value={
-                        textEditedTask !== null ? textEditedTask : todo.text
-                      }
-                      onChange={todoEditHandler}
-                      className={`${styles.input} form-control`}
-                    />
-                  </div>
+                  <input
+                    id="editedTask"
+                    type="text"
+                    required
+                    value={
+                      enteredTitleEditedTask !== null
+                        ? enteredTitleEditedTask
+                        : task.title
+                    }
+                    onChange={titleChangeEditedTaskHandler}
+                    className={`form-change-title_control`}
+                  />
                 </div>
-                <div className="">
-                  <button
-                    id="submit-form"
-                    type="submit"
-                    className={styles.button}
-                  >
-                    save
-                  </button>
-                </div>
+
+                <button
+                  id="submit-form"
+                  type="submit"
+                  className={styles.button}
+                >
+                  save
+                </button>
               </form>
             );
           })}
-      </div>
+      </ul>
 
       <button
         onClick={() => {
@@ -212,14 +253,14 @@ const Home: NextPage = () => {
       </button>
       <button
         onClick={() => {
-          setTasksTypeToDisplay(TaskType.Undone);
+          setTasksTypeToDisplay(TaskType.Uncompleted);
         }}
       >
         TODO
       </button>
       <button
         onClick={() => {
-          setTasksTypeToDisplay(TaskType.Done);
+          setTasksTypeToDisplay(TaskType.Completed);
         }}
       >
         DONE
